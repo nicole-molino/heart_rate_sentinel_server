25b30d5afb0be427c1e0a7bcd37a7fb8fe11a152
Nicole Molino <nkm12@duke.edu>
2018-11-15 16:51:33 -0500

    Sucessfully determine if tachy and send email

--- new_p.py
+++ new_p.py
@@ -1,4 +1,4 @@
-from builtins import int
+from builtins import int, type
 from statistics import mean
 from flask import Flask, jsonify, request
 from pymodm import connect
@@ -7,6 +7,7 @@ import datetime
 import logging
 
 from determine_if_tachy import determine_if_tachy
+from send_grid import send_email
 from validate_get_heart_rate import validate_get_heart_rate, ValidationError
 
 # from validate_patient_id import validate_patient_id
@@ -55,6 +56,9 @@ def add_new_p():
                    user_age=a["user_age"])
 
     patient.save()
+
+
+
     logging.info("Added a new patient, %s", a["patient_id"])
 
     result = {"message": "Successfully added new patient"}
@@ -80,7 +84,21 @@ def add_HR():
     logging.info("Added HR (%s BPM), patient: %s,"
                  "  time: %s", a["heart_rate"], a["patient_id"], now)
 
+
     result = {"message": "Successfully added heart rate data"}
+
+    HR = int(a["heart_rate"])
+
+    for user in User.objects.raw({"_id": a["patient_id"]}):
+        age = int(user.user_age)
+
+    try:
+        answer = determine_if_tachy(age, HR)
+        if answer:
+            send_email()
+    except UnboundLocalError:
+        raise ValidationError("User does not exist")
+        logging.warning("Tried to access user that does not exist")
 
     return jsonify(result)
 
@@ -96,13 +114,21 @@ def get_heart_rate(patient_id):
             try:
                 validate_get_heart_rate(user.heart_rate)
                 logging.info("Add heart rate data %s", user.heart_rate)
+
+
             except ValidationError:
                 return jsonify("User exists but no heart rate data")
 
         return jsonify(user.heart_rate)
+
+
     except UnboundLocalError:
         raise ValidationError("User does not exist")
         logging.warning("Tried to access user that does not exist")
+
+
+
+
 
 
 @app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
@@ -126,14 +152,14 @@ def calculate_avg_HR(patient_id):
         logging.warning("Tried to access user that does not exist")
 
 
-@app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
-def calculate_avg_HR(patient_id):
+@app.route("/api/heart_rate/status/<patient_id>", methods=["GET"])
+def determine_tachy(patient_id):
     connect("mongodb://bme590:hello12345@ds157818.mlab.com:57818/hr")
 
     a = int(patient_id)
 
     try:
-        for user in User.objectd({"_id": a}):
+        for user in User.objects.raw({"_id": a}):
             try:
                 validate_get_heart_rate(user.heart_rate)
             except ValidationError:
@@ -142,8 +168,17 @@ def calculate_avg_HR(patient_id):
 
         age = int(user.user_age)
         HR = int(user.heart_rate[-1])
+        time = user.time_stamp[-1]
         answer = determine_if_tachy(age, HR)
-        return jsonify(answer)
+
+        if answer:
+            send_email()
+            ans_str= 'Tachycardic'
+        else:
+             ans_str= 'Not tachycardic'
+
+        return jsonify(ans_str,time)
+
 
     except UnboundLocalError:
         raise \
