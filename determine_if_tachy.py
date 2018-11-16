43b5149fe3a6c05ba3fd25da9ccf95beaed05bca
Nicole Molino <nkm12@duke.edu>
2018-11-16 15:01:34 -0500

    Add docstrings

--- determine_if_tachy.py
+++ determine_if_tachy.py
@@ -2,59 +2,58 @@ from builtins import ValueError
 
 
 def determine_if_tachy(age, HR):
+    """
+    Determine if patient is tachycardic
+
+    Args:
+        age (float): age of patient
+        HR (float) : HR of patient
+
+    Returns:
+        a: connditional if tachy or not
+
+    Raises:
+        ValueError: patient younger than 2 months
+
+    """
+    a = 0
     if age < (1 / 12):
-        raise ValueError("Age must be greater than 2 months. Retry")
+        raise ValueError("Age must be greater than 2 months")
 
     if age >= (1 / 12) and age < (3 / 12):
         if HR > 179:
-            return 1
-        else:
-            return 0
+            a=1
 
     if age >= (3 / 12) and age <= (6 / 12):
         if HR > 186:
-            return 1
-        else:
-            return 0
+            a =1
 
     if age >= (6 / 12) and age < 1:
         if HR > 169:
-            return 1
-        else:
-            return 0
+            a = 1
 
     if age >= 1 and age < 3:
         if HR > 151:
-            return 1
-        else:
-            return 0
+            a = 1
 
     if age >= 3 and age < 5:
         if HR > 137:
-            return 1
-        else:
-            return 0
+            a = 1
 
     if age >= 5 and age < 8:
         if HR > 133:
-            return 1
-        else:
-            return 0
+            a = 1
 
     if age >= 8 and age < 12:
         if HR > 130:
-            return 1
-        else:
-            return 0
+            a = 1
 
     if age >= 12 and age < 15:
         if HR > 119:
-            return 1
-        else:
-            return 0
+            a = 1
 
     if age >= 15:
         if HR > 100:
-            return 1
-        else:
-            return 0
+            a = 1
+
+    return a
